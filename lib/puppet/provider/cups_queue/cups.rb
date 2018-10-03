4dc905ddd08d0de0afdc6737b9472abfa3b0e752
Helen Griffiths <threepistons@users.noreply.github.com>
2018-10-03 16:34:51 +0100

    increase time between setting options

--- lib/puppet/provider/cups_queue/cups.rb
+++ lib/puppet/provider/cups_queue/cups.rb
@@ -199,7 +199,7 @@ Puppet::Type.type(:cups_queue).provide(:cups) do
   def options=(options_should)
     options_should.each do |key, value|
       lpadmin('-E', '-p', name, '-o', "#{key}=#{value}")
-      sleep(2)
+      sleep(5)
     end
   end
 
